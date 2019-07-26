014ed22244bf3026b7b27e144a041d213c9a3f0a
Ryland McGee <mcgee.reilly@gmail.com>
2019-07-26 14:06:18 -0800

    added nota-pos

---
+++ precache-manifest.bf58f8cbc6e7d6dc3893c343ef427c8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6beb2c40d717029e95863c4538a32111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3551c521f919747896",
+    "url": "/static/css/main.ae83e8f3.chunk.css"
+  },
+  {
+    "revision": "8e79117a070915a18775",
+    "url": "/static/js/2.75293adf.chunk.js"
+  },
+  {
+    "revision": "6a3551c521f919747896",
+    "url": "/static/js/main.c4230d21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9432d62df4b40f53c8daa79acca4039d",
+    "url": "/static/media/CriticalFailDemo.9432d62d.png"
+  },
+  {
+    "revision": "0daab090fcf572e1b2d3567565639736",
+    "url": "/static/media/HorizontalImage.0daab090.jpg"
+  },
+  {
+    "revision": "72453d877a8bfe5ae22f8d97feec3c2f",
+    "url": "/static/media/Nota-POSDemo.72453d87.png"
+  },
+  {
+    "revision": "9f9d39435fcac81caf03a7e00851667a",
+    "url": "/static/media/verticalImage.9f9d3943.jpg"
+  }
+]);
